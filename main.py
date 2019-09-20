3814d8863ff8d336e74f2a128b1d7208930c5805
Nithin Sade <nithinsade@Nithins-MacBook-Pro.local>
2019-09-20 01:15:40 -0400

    login made

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, send_from_directory, jsonify, request, redirect, url_for
+from flask import Flask, send_from_directory, jsonify, request, redirect, url_for, make_response
 from google.cloud import datastore
+from random import randint
 
 app = Flask(__name__, static_url_path='/static')
-client = datastore.Client('remind-me-663')
+# client = datastore.Client('remind-me-663')
+client = datastore.Client('remind-me-1089')
 
 # e = {
 #     "events": [{
@@ -14,29 +16,117 @@ client = datastore.Client('remind-me-663')
 #     }]
 # }
 
+creds = {
+    'username':None,
+    'sessionID':None
+}
+
+def rand():
+    return randint(0, 10000)
+
+def auth(user_input,db_input):
+    if user_input == db_input:
+        return True
+    else:
+        return False
+
+
 @app.route('/', methods=['GET'])
 def index():
+    # todo: Make a check for session cookie!
+    print(creds)
+    if request.cookies.get('username') == None:
+        print(request.cookies.get('username'))
+        return app.send_static_file('login.html')
+    print(request.cookies.get('username'))
+    creds['username'] = request.cookies.get('username')
     return app.send_static_file('index.html')
 
-@app.route('/events')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    print(creds)
+    
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        key = client.key('users', username)
+        if(client.get(key)):
+            data = client.get(key)
+            test = data['password']
+            
+            if(auth(password,test)):
+                creds['username'] = username
+                creds['sessionID'] = data['sessionID']
+                resp = make_response(redirect(url_for('index')))
+                resp.set_cookie("username", username, max_age=60 * 60 * 24)
+                return resp
+            else:
+                return 'Wrong Creds' # ! NEED to change this
+                
+    else:
+        return app.send_static_file('login.html')
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    resp = make_response(redirect(url_for('index')))
+    resp.set_cookie('username', expires=0)
+    return resp
+
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+     # todo: Set Session cookie if username does not exist!
+    if request.method == 'GET':
+        return app.send_static_file('signup.html')
+   
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        random = rand()
+        complete_key = client.key('users', username)
+        task = datastore.Entity(key=complete_key)
+        task.update({
+            'username':username,
+            'password':password,
+            'sessionID': random
+        })
+        creds['username'] = username
+        creds['sessionID'] = random
+        print(task)
+        client.put(task)
+        resp = make_response(redirect(url_for('index')))
+        resp.set_cookie("username", username, max_age=60 * 60 * 24)
+        return resp
+
+
+@app.route('/events', methods=['POST','GET'])
 def events():
     # ! Data Store section
-    query = client.query(kind='events')
-    results = list(query.fetch())
-    keys = ['id','date','name']
-    d = dict.fromkeys(keys, None)
-    arr = []
-
-    for result in results:
+    # todo: get events of that username
+    # query = client.query(kind='events')
+    # query.add_filter("username", "=" , "sunny")
+    if (request.cookies.get('username')):
+        parent_key = client.key('users', request.cookies.get('username'))
+        query = client.query(kind='events', ancestor=parent_key)
+        results = list(query.fetch())
+        keys = ['id','date','name']
         d = dict.fromkeys(keys, None)
-        d['id'] = result.id
-        d['name']=result['name']
-        d['date']=result['date']
-        arr.append(d)
+        arr = []
+        #print(results)
+        for result in results:
+            d = dict.fromkeys(keys, None)
+            d['id'] = result.id
+            d['name']=result['name']
+            d['date']=result['date']
+            arr.append(d)
     # print(arr)
     # ! *****************
-    return jsonify({'events': arr})
-    #return jsonify(e)
+        return jsonify({'events': arr})
+    else:
+        return redirect(url_for('index'))
 
 @app.route('/event',methods=['POST'])
 def AddEvent():
@@ -45,9 +135,11 @@ def AddEvent():
     d = {
         "date": date,
         "name": name
+        # "id" : rand()
     }
     # ! Data Store Implementation
-    query = client.query(kind='events')
+    parent_key = client.key('users', request.cookies.get('username'))
+    query = client.query(kind='events', ancestor=parent_key)
     results = list(query.fetch())
     #print(results)
     for result in results:
@@ -57,19 +149,18 @@ def AddEvent():
             return redirect(url_for('index'))
         else:
             pass
-    key = client.key('events')
-    item = datastore.Entity(key)
-    item.update(d)
-    key = client.put(item)
+    key = client.key('events',parent=parent_key)
+    task = datastore.Entity(key=key)
+    task.update(d)
+    client.put(task)
     return redirect(url_for('index'))
     # ! **********************
     
-    
-
 @app.route('/delete/<key>',methods=['POST'])
 def DeleteEvent(key):
     # ! Data Store Implementation
-    key = client.key('events', int(key))
+    parent_key = client.key('users', creds['username'])
+    key = client.key('events', int(key), parent=parent_key)
     deleted = client.delete(key)
     print(key)
     # ! **********************
