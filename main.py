fc08e140eae848c51c99ad9910efb5acc8614131
Nithin Sade <nithinsade@Nithins-MacBook-Pro.local>
2019-09-25 02:55:42 -0400

    resolved caching issues

--- main.py
+++ main.py
@@ -5,43 +5,27 @@ import bcrypt
 import logging
 
 app = Flask(__name__, static_url_path='/static')
-# client = datastore.Client('remind-me-663')
-client = datastore.Client('remind-me-1089')
+#Datastore
+# client = datastore.Client('remind-me-1089')
+client = datastore.Client('remind-me-2lab')
 
-def rand(): # Generating session
-    return str(randint(100000000000,999999999999))
-
-def auth(user_input, db_input, salt): #Checking hash
-    test = bcrypt.hashpw(user_input.encode(), salt)
-
-    if test == db_input:
-        return True
-    else:
-        return False
-
-def hasher(pwd): #hashing the user inout password
-    password = pwd.encode()
-    salt = bcrypt.gensalt(10)
-    hashed = bcrypt.hashpw(password, salt)
-    return hashed , salt
-
-
-
-@app.route('/', methods=['GET'])
-def index():
+@app.route('/', methods=['GET','POST'])
+def root():
     logging.warning('This is an error message')
-    if request.cookies.get('sessionID') == None:
-        # print(request.cookies.get('sessionID'))
-        return app.send_static_file('login.html')
+    if (request.cookies.get('sessionID') == None):
+        logging.warning(request.cookies.get('sessionID'))
+        return redirect(url_for('login'))
 
-    query = client.query(kind='users')
-    query.add_filter('sessionID','=',request.cookies.get('sessionID'))  
+    else:
+        query = client.query(kind='users')
+        query.add_filter('sessionID','=',request.cookies.get('sessionID'))  
 
-    if(list(query.fetch()) == []):
-          return app.send_static_file('login.html')
-
-    # print(request.cookies.get('sessionID'))
-    return app.send_static_file('index.html')
+        if(list(query.fetch()) == []):
+            return redirect(url_for('login'))
+        else:
+            logging.warning(request.cookies.get('sessionID'))
+            return app.send_static_file('home.html')
+    return redirect(url_for('login'))
 
 
 @app.route('/login', methods=['GET','POST'])
@@ -64,23 +48,21 @@ def login():
                 
                 client.put(task)
 
-                resp = make_response(redirect(url_for('index')))
+                resp = make_response(redirect(url_for('root')))
                 resp.set_cookie("sessionID", random, max_age=60 * 60 * 1) # Setting cookie time 1 hour
-                return resp
+                return resp, 307
             else:
                 return app.send_static_file('login.html')
-        
         else:
-            return app.send_static_file('login.html')
-                
+            return app.send_static_file('login.html')               
     else:
         return app.send_static_file('login.html')
 
 @app.route('/logout', methods=['POST']) # Expiring the cookie
 def logout():
-    resp = make_response(redirect(url_for('index')))
-    resp.set_cookie('sessionID', expires=0)
-    return resp
+    resp = make_response(redirect(url_for('root')))
+    resp.set_cookie('sessionID','', expires=0)
+    return resp, 307
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -103,17 +85,13 @@ def register():
             'sessionID':random
         })
         client.put(task)
-        resp = make_response(redirect(url_for('index')))
+        resp = make_response(redirect(url_for('root')))
         resp.set_cookie("sessionID", random, max_age=60 * 60 * 1) # Setting cookie time 1 hour
-        return resp
-
+        return resp, 307
 
 @app.route('/events', methods=['POST','GET'])
 def events():
     # ! Data Store section
-    # todo: get events of that username
-    # query = client.query(kind='events')
-    # query.add_filter("username", "=" , "sunny")
     if (request.cookies.get('sessionID')):
         query = client.query(kind='users')
         query.add_filter('sessionID','=',request.cookies.get('sessionID'))
@@ -121,8 +99,6 @@ def events():
         if(list(query.fetch()) != []):
             result = list(query.fetch())
             parent_key = result[0].key
-
-            # parent_key = client.key('users', request.cookies.get('username'))
             query = client.query(kind='events', ancestor=parent_key)
             results = list(query.fetch())
             keys = ['id','date','name']
@@ -139,9 +115,9 @@ def events():
         # ! *****************
             return jsonify({'events': arr})
         else:
-            return redirect(url_for('index'))
+            return redirect(url_for('root'))
     else:
-        return redirect(url_for('index'))
+        return redirect(url_for('root'))
 
 @app.route('/event',methods=['POST'])
 def AddEvent():
@@ -166,14 +142,14 @@ def AddEvent():
         #print(result['name'])
         if str(result['name']) == str(name):
             print("Do not use duplicate event names!!")
-            return redirect(url_for('index'))
+            return redirect(url_for('root'))
         else:
             pass
     key = client.key('events',parent=parent_key)
     task = datastore.Entity(key=key)
     task.update(d)
     client.put(task)
-    return redirect(url_for('index'))
+    return redirect(url_for('root'))
     # ! **********************
     
 @app.route('/delete/<key>',methods=['POST'])
@@ -189,10 +165,36 @@ def DeleteEvent(key):
     deleted = client.delete(key)
     # print(key)
     # ! **********************
-    return redirect(url_for('index'))
+    return redirect(url_for('root'))
 
-@app.route('/sucess')
+@app.route('/sucess',methods=['GET'])
 def DeleteSuccess():
+    logging.warning(request.cookies.get('sessionID'))
     return 'Deletion Successfull'
+
+def rand(): # Generating session
+    return str(randint(100000000000,999999999999))
+
+def auth(user_input, db_input, salt): #Checking hash
+    test = bcrypt.hashpw(user_input.encode(), salt)
+
+    if test == db_input:
+        return True
+    else:
+        return False
+
+def hasher(pwd): #hashing the user inout password
+    password = pwd.encode()
+    salt = bcrypt.gensalt(10)
+    hashed = bcrypt.hashpw(password, salt)
+    return hashed , salt
+
+@app.after_request
+def set_response_headers(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
